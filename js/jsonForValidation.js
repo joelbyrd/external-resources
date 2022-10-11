1559bc31386611c9eaf3f745a72854113c805f2a
joelbyrd <joelbyrd@gmail.com>
2022-10-11 17:19:44 -0400

    Create jsonForValidation.js

---
+++ js/jsonForValidation.js
@@ -0,0 +1,405 @@
+export const JSON_OLD = [
+	{
+		accessType: "",
+		apiProducts: [],
+		appFamily: "default",
+		appId: "d8547648-858d-4a59-a3c0-0b700056f93a",
+		attributes: [
+			{
+				name: "AllowedServices",
+				value: "~ALL~"
+			},
+			{
+				name: "AllowedStores",
+				value: "~MV0000002151~"
+			},
+			{
+				name: "Version",
+				value: "v1.0"
+			},
+			{
+				name: "DisplayName",
+				value: "MV0000002151 Schema Validation"
+			},
+			{
+				name: "Notes",
+				value:
+					"MV0000002151 This is a mapping for the Provisioning Integration Center"
+			}
+		],
+		callbackUrl: "",
+		createdAt: 1522164223521,
+		createdBy: "opdk@radial.com",
+		credentials: [
+			{
+				apiProducts: [
+					{
+						apiproduct: "Tax-Core",
+						status: "approved"
+					},
+					{
+						apiproduct: "Payments-Core",
+						status: "approved"
+					},
+					{
+						apiproduct: "Risk-Core",
+						status: "approved"
+					}
+				],
+				attributes: [],
+				consumerKey: "P0Z3Q0nn4LXAkCV4j2tFq4R6BBX9G2V8",
+				consumerSecret: "rKXO3Ew672VcgiEj",
+				expiresAt: -1,
+				issuedAt: 1522164223759,
+				scopes: [],
+				status: "approved"
+			}
+		],
+		developerId: "6RlGPxGERpNAqV85",
+		lastModifiedAt: 1522164223521,
+		lastModifiedBy: "opdk@radial.com",
+		name: "MV0000002151",
+		scopes: [],
+		status: "approved"
+	},
+	{
+		accessType: "",
+		apiProducts: [],
+		appFamily: "default",
+		appId: "f1eb2b1d-0324-4293-9d61-b0fa8f533b3b",
+		attributes: [
+			{
+				name: "AllowedServices",
+				value: "~ALL~"
+			},
+			{
+				name: "AllowedStores",
+				value: "~PM0000002002~"
+			},
+			{
+				name: "Version",
+				value: "v1.0"
+			},
+			{
+				name: "DisplayName",
+				value: "PM0000002002 Schema Validation"
+			},
+			{
+				name: "Notes",
+				value:
+					"PM0000002002 This is a mapping for the Provisioning Integration Center"
+			}
+		],
+		callbackUrl: "",
+		createdAt: 1522164293475,
+		createdBy: "opdk@radial.com",
+		credentials: [
+			{
+				apiProducts: [
+					{
+						apiproduct: "Tax-Core",
+						status: "approved"
+					},
+					{
+						apiproduct: "Payments-Core",
+						status: "approved"
+					},
+					{
+						apiproduct: "Risk-Core",
+						status: "approved"
+					}
+				],
+				attributes: [],
+				consumerKey: "c0yukr0DAGUqwlcesRW7UzPCKFkd1cbG",
+				consumerSecret: "V91q1IKs4nLjhuY3",
+				expiresAt: -1,
+				issuedAt: 1522164293740,
+				scopes: [],
+				status: "approved"
+			}
+		],
+		developerId: "6RlGPxGERpNAqV85",
+		lastModifiedAt: 1522164293475,
+		lastModifiedBy: "opdk@radial.com",
+		name: "PM0000002002",
+		scopes: [],
+		status: "approved"
+	},
+	{
+		accessType: "",
+		apiProducts: [],
+		appFamily: "default",
+		appId: "92b00349-2e77-4b0a-808b-f245bce06d33",
+		attributes: [
+			{
+				name: "AllowedServices",
+				value: "~ALL~"
+			},
+			{
+				name: "AllowedStores",
+				value: "~AB0000151061~"
+			},
+			{
+				name: "Version",
+				value: "v1.0"
+			},
+			{
+				name: "DisplayName",
+				value: "AB0000151061 Schema Validation"
+			},
+			{
+				name: "Notes",
+				value:
+					"AB0000151061 This is a mapping for the Provisioning Integration Center"
+			}
+		],
+		callbackUrl: "",
+		createdAt: 1522162583618,
+		createdBy: "opdk@radial.com",
+		credentials: [
+			{
+				apiProducts: [
+					{
+						apiproduct: "Tax-Core",
+						status: "approved"
+					},
+					{
+						apiproduct: "Payments-Core",
+						status: "approved"
+					},
+					{
+						apiproduct: "Risk-Core",
+						status: "approved"
+					}
+				],
+				attributes: [],
+				consumerKey: "JzLtVMXwS69tRyPWz0PuscdRoTfqTwfk",
+				consumerSecret: "nDh3HqQ0Ol4HzzvB",
+				expiresAt: -1,
+				issuedAt: 1522162583810,
+				scopes: [],
+				status: "approved"
+			}
+		],
+		developerId: "6RlGPxGERpNAqV85",
+		lastModifiedAt: 1522162583618,
+		lastModifiedBy: "opdk@radial.com",
+		name: "AB0000151061",
+		scopes: [],
+		status: "approved"
+	}
+];
+
+export const JSON_NEW = [
+	{
+		appId: "3256c465-d5cc-4ad3-a2df-bad1f1f0a682",
+		attributes: [
+			{
+				name: "AllowedInterval",
+				value: "1"
+			},
+			{
+				name: "AllowedQuota",
+				value: "1000pm"
+			},
+			{
+				name: "AllowedServices",
+				value: "~ALL~"
+			},
+			{
+				name: "AllowedStores",
+				value: "~LUXGB~"
+			},
+			{
+				name: "AllowedTimePeriod",
+				value: "month"
+			},
+			{
+				name: "DisplayName",
+				value: "LUX-eBay"
+			},
+			{
+				name: "Version",
+				value: "v1.0"
+			}
+		],
+		createdAt: 1646021258608,
+		credentials: [
+			{
+				apiProducts: [
+					{
+						apiproduct: "Tax-Core",
+						status: "approved"
+					},
+					{
+						apiproduct: "ivrproduct",
+						status: "approved"
+					},
+					{
+						apiproduct: "Payments-Core",
+						status: "approved"
+					},
+					{
+						apiproduct: "returnscanapi",
+						status: "approved"
+					},
+					{
+						apiproduct: "Verify-API-Key",
+						status: "approved"
+					},
+					{
+						apiproduct: "Risk-Core",
+						status: "approved"
+					},
+					{
+						apiproduct: "returnlabel",
+						status: "approved"
+					},
+					{
+						apiproduct: "epapi",
+						status: "approved"
+					}
+				],
+				consumerKey: "b1d7c3f46c58faa77aee13c8e6f318e0",
+				consumerSecret: "AlP9NLoY6s8IlVI6",
+				expiresAt: -1,
+				issuedAt: 1646075771347,
+				status: "approved"
+			}
+		],
+		developerId: "2cec094b-acbf-45d6-81f1-e0e79f00d7eb",
+		lastModifiedAt: 1646021258608,
+		name: "LUX-eBay",
+		status: "approved"
+	},
+	{
+		appId: "2542102e-9f87-49fb-b112-a11029c6522f",
+		attributes: [
+			{
+				name: "AllowedInterval",
+				value: "1"
+			},
+			{
+				name: "AllowedQuota",
+				value: "1000pm"
+			},
+			{
+				name: "AllowedServices",
+				value: "~ALL~"
+			},
+			{
+				name: "AllowedStores",
+				value: "~LUXGB~"
+			},
+			{
+				name: "AllowedTimePeriod",
+				value: "month"
+			},
+			{
+				name: "DisplayName",
+				value: "LUX-Cust"
+			},
+			{
+				name: "Version",
+				value: "v1.0"
+			}
+		],
+		createdAt: 1646021255977,
+		credentials: [
+			{
+				apiProducts: [
+					{
+						apiproduct: "Tax-Core",
+						status: "approved"
+					},
+					{
+						apiproduct: "ivrproduct",
+						status: "approved"
+					},
+					{
+						apiproduct: "Payments-Core",
+						status: "approved"
+					},
+					{
+						apiproduct: "returnscanapi",
+						status: "approved"
+					},
+					{
+						apiproduct: "Verify-API-Key",
+						status: "approved"
+					},
+					{
+						apiproduct: "Risk-Core",
+						status: "approved"
+					},
+					{
+						apiproduct: "returnlabel",
+						status: "approved"
+					},
+					{
+						apiproduct: "epapi",
+						status: "approved"
+					}
+				],
+				consumerKey: "25a9d347efecc1205e37ffe902412a97",
+				consumerSecret: "AlP9NLoY6s8IlVI6",
+				expiresAt: -1,
+				issuedAt: 1646075768785,
+				status: "approved"
+			}
+		],
+		developerId: "2cec094b-acbf-45d6-81f1-e0e79f00d7eb",
+		lastModifiedAt: 1646021255977,
+		name: "LUX-Cust",
+		status: "approved"
+	},
+	{
+		appId: "58c19fea-6511-45ba-ad9b-f635aaddd506",
+		attributes: [
+			{
+				name: "AllowedServices",
+				value: "~ALL~"
+			},
+			{
+				name: "AllowedStores",
+				value: "~TMSB~~TRUS~"
+			},
+			{
+				name: "DisplayName",
+				value: "endtoendtest"
+			},
+			{
+				name: "Version",
+				value: "v1.0"
+			}
+		],
+		createdAt: 1646020834932,
+		credentials: [
+			{
+				apiProducts: [
+					{
+						apiproduct: "LoadTestProduct",
+						status: "approved"
+					},
+					{
+						apiproduct: "EnterpriseSearchApp",
+						status: "approved"
+					},
+					{
+						apiproduct: "Verify-API-Key",
+						status: "approved"
+					}
+				],
+				consumerKey: "mTlFfIAN1ObAYGZp5hywLWQQbP4EBOmb",
+				consumerSecret: "pHhez37r6V4GHiI2",
+				expiresAt: -1,
+				issuedAt: 1646020837719,
+				status: "approved"
+			}
+		],
+		developerId: "5db90b24-3d20-486f-a189-58cc12119b8d",
+		lastModifiedAt: 1646020834932,
+		name: "endtoendtest",
+		status: "approved"
+	}
+];
